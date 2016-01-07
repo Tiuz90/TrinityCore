5e3ac2317c2356813d52f248c989a1069f1cd78f
Yehonal <hw.2@hotmail.it>
2016-01-08 00:43:46 +0100

    improved comments

--- azth/data/sql/tools/azth_char_transfer.sql
+++ azth/data/sql/tools/azth_char_transfer.sql
@@ -5,10 +5,13 @@ UPDATE account_transfer SET cDump = "CLEANED", cItemRow = "CLEANED" WHERE date_c
 
 # sometime, after accepting a transfer, the ajax can be fail for various 
 # reasons stucking the characters in porting account.
-# this query can be used to "fix" characters affected by this error
-
+# following query can be used to "fix" characters affected by this error
 
 UPDATE azth_1_chars.characters AS a RIGHT JOIN (
-SELECT guid,cAccount FROM azth_auth.account_transfer WHERE guid IN (SELECT guid FROM azth_1_chars.characters WHERE account = 523) AND cStatus = 1
+SELECT guid,cAccount FROM azth_auth.account_transfer WHERE
+/* conditions must follow this order for best performances */
+guid IN (SELECT guid FROM azth_1_chars.characters WHERE account = 523)
+AND cStatus = 1
 ) AS b
 ON a.guid=b.GUID SET a.account=b.cAccount;
+
